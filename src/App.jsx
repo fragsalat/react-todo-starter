8acaf45a32604d57211e7e8aa207784c2473ec56
Thomas <thomas.schlage@zalando.de>
2023-12-18 15:55:13 +0100

    Added template

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import Hello from './Hello'
+
+function App() {
+  return (
+    <Hello />
+  )
+}
+
+export default App
---
+++ src/Hello.jsx
@@ -0,0 +1,16 @@
+export default ({ name }) => (
+  <div className="hello">
+    <h1>Hey!</h1>
+    <p>
+      Your task is to create a simple todo application. A todo consists of the
+      task: string and a flag done: boolean. You can add and delete todos and
+      mark them as done.
+    </p>
+    <strong>Requirements</strong>
+    <ul>
+      <li>User can add new todo with input field and submit button</li>
+      <li>User can delete todo by clicking a trashbin icon 🗑</li>
+      <li>Optional: User can mark todo as done by clicking a checkbox</li>
+    </ul>
+  </div>
+);
